eb6d1d024fabefa83927f43880e739123cc7aa14
COBSCComp20.1P-029 - R.M.G.S.Rathnayaka <gayarathnayaka96@gmail.com>
2021-11-10 23:09:54 -0800

    Initial Commit

---
+++ NIBM Parking/NIBM_ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIBM_ParkingApp.swift
+//  NIBM Parking
+//
+//  Created by Gayathree Rathnayaka on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct NIBM_ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
